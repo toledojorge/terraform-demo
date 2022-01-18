35cff8f0dd806b8fc48120323d03fb99e68c9425
TOLEDO BARANDICA <j.toledo.barandica@accenture.com>
2022-01-18 12:24:10 -0500

    usando provisioner

--- main.tf
+++ main.tf
@@ -9,10 +9,15 @@ terraform {
   }
 }
 
-provider "docker" {}
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
 
 resource "docker_container" "ubuntu" {
     name = "ubuntu1"
     image = "ubuntu:18.04"
     command = ["/bin/bash","-c","while true; do sleep 60000; done"]
+    provisioner "local-exec" {
+      command = "echo ${docker_container.ubuntu.network_data[0].ip_address} >> inventario.txt"
+    }
 }
