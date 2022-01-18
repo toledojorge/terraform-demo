cac302ff400ab60c20b7049c89abf1b238876d3e
TOLEDO BARANDICA <j.toledo.barandica@accenture.com>
2022-01-18 11:01:23 -0500

    ajuste terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
 provider "docker" {}
 
 resource "docker_container" "ubuntu" {
