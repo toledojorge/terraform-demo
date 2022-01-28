635031580069738d5f94fca6c5fadde9c35c9ed8
TOLEDO BARANDICA <j.toledo.barandica@accenture.com>
2022-01-28 06:52:07 -0500

    bien

--- aws/linkedin/prod.tf
+++ aws/linkedin/prod.tf
@@ -53,3 +53,11 @@ resource "aws_instance" "prod_web" {
         "Terraform" : "true"
     }
 }
+
+resource "aws_eip" "prod_web" {
+    instance = aws_instance.prod_web.id
+
+    tags = {
+        "Terraform" : "true"
+    }
+}
