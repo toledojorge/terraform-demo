e3e7cc0c97b724c2f544701546565ec5ac33757c
TOLEDO BARANDICA <j.toledo.barandica@accenture.com>
2022-01-28 15:02:37 -0500

    ajuste variables

--- aws/linkedin/prod.tf
+++ aws/linkedin/prod.tf
@@ -1,6 +1,20 @@
-variable "webserver_ami" {
+variable "whitelist" {
+    type = list(string)
+}
+variable "web_image_id" {
     type = string
-    default = "ami-0ab3b071083dddbb8"
+}
+variable "web_instance_type" {
+    type = string
+}
+variable "web_desired_capacity" {
+    type = number
+}
+variable "web_max_size" {
+    type = number
+}
+variable "web_min_size" {
+    type = number
 }
 
 provider "aws" {
@@ -43,21 +57,21 @@ resource "aws_security_group" "prod_web" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.whitelist
     }
 
     ingress {
         from_port = 443
         to_port = 443
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.whitelist
     }
 
     egress {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = var.whitelist
     }
 
     tags = {
@@ -85,8 +99,8 @@ resource "aws_elb" "prod_web" {
 # Template para re usar creacion instancias EC2
 resource "aws_launch_template" "prod_web" {
     name_prefix = "prod-web"
-    image_id = var.webserver_ami
-    instance_type = "t2.micro"
+    image_id = var.web_image_id
+    instance_type = var.web_instance_type
     tags = {
         "Terraform" : "true"
     }
@@ -95,9 +109,9 @@ resource "aws_launch_template" "prod_web" {
 # Autoscaling group
 resource "aws_autoscaling_group" "prod_web" {    
     vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
-    desired_capacity = 1
-    max_size = 1
-    min_size = 1
+    desired_capacity = var.web_desired_capacity
+    max_size = var.web_max_size
+    min_size = var.web_min_size
 
     launch_template {
         id = aws_launch_template.prod_web.id
