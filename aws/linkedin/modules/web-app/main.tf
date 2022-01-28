008db872fd1679d1b26aec7fadb673c3a663a7b1
TOLEDO BARANDICA <j.toledo.barandica@accenture.com>
2022-01-28 15:59:23 -0500

    creando modulos

---
+++ aws/linkedin/modules/web-app/main.tf
@@ -0,0 +1,49 @@
+# Balanceador de carga
+resource "aws_elb" "this" {
+    name = "${web_app}-web"    
+    subnets = var.subnets
+    security_groups = var.security_groups 
+    listener {
+        instance_port       = 80
+        instance_protocol   = "http"
+        lb_port             = 80
+        lb_protocol         = "http"
+    }
+    tags = {
+        "Terraform" : "true"
+    }
+}
+
+# Template para re usar creacion instancias EC2
+resource "aws_launch_template" "this" {
+    name_prefix = "${web_app}-web"
+    image_id = var.web_image_id
+    instance_type = var.web_instance_type
+    tags = {
+        "Terraform" : "true"
+    }
+}
+
+# Autoscaling group
+resource "aws_autoscaling_group" "this" {    
+    vpc_zone_identifier = var.subnets
+    desired_capacity = var.web_desired_capacity
+    max_size = var.web_max_size
+    min_size = var.web_min_size
+
+    launch_template {
+        id = aws_launch_template.this.id
+        version = "$Latest"
+    }
+
+    tag {
+        key                 = "Terraform"
+        value               = "true"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_autoscaling_attachment" "this" {
+    autoscaling_group_name = aws_autoscaling_group.this.id
+    elb = aws_elb.this.id
+}
--- aws/linkedin/prod.tf
+++ aws/linkedin/prod.tf
@@ -25,6 +25,9 @@ provider "aws" {
 resource "aws_s3_bucket" "prod_tf_course" {
     bucket = "tf-course-20220127"
     acl = "private"
+    tags = {
+        "Terraform" : "true"
+    }
 }
 
 # Default VPC
@@ -77,55 +80,4 @@ resource "aws_security_group" "prod_web" {
     tags = {
         "Terraform" : "true"
     }
-}
-
-
-# Balanceador de carga
-resource "aws_elb" "prod_web" {
-    name = "prod-web"    
-    subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
-    security_groups = [aws_security_group.prod_web.id]
-    listener {
-        instance_port       = 80
-        instance_protocol   = "http"
-        lb_port             = 80
-        lb_protocol         = "http"
-    }
-    tags = {
-        "Terraform" : "true"
-    }
-}
-
-# Template para re usar creacion instancias EC2
-resource "aws_launch_template" "prod_web" {
-    name_prefix = "prod-web"
-    image_id = var.web_image_id
-    instance_type = var.web_instance_type
-    tags = {
-        "Terraform" : "true"
-    }
-}
-
-# Autoscaling group
-resource "aws_autoscaling_group" "prod_web" {    
-    vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
-    desired_capacity = var.web_desired_capacity
-    max_size = var.web_max_size
-    min_size = var.web_min_size
-
-    launch_template {
-        id = aws_launch_template.prod_web.id
-        version = "$Latest"
-    }
-
-    tag {
-        key                 = "Terraform"
-        value               = "true"
-        propagate_at_launch = true
-    }
-}
-
-resource "aws_autoscaling_attachment" "prod_web" {
-    autoscaling_group_name = aws_autoscaling_group.prod_web.id
-    elb = aws_elb.prod_web.id
 }
